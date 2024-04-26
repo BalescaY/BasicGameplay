73f11513d1eb2bdf68be7a03b958080fe111a596
BalescaY <martineztavarezy@roverkids.org>
2024-04-26 14:20:08 -0400

    2.2 almost done

--- BasicGameplay/Assets/Scripts/PlayerController.cs
+++ BasicGameplay/Assets/Scripts/PlayerController.cs
@@ -7,18 +7,21 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
         if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);  
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
 
         if (transform.position.x > xRange)
@@ -28,6 +31,18 @@ public class PlayerController : MonoBehaviour
 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+
+
+     
         
+        }
     }
 }
+
+        
+      
+        
